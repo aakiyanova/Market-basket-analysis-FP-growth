d14e7a29119aa6bbd63b0759940a3761427c4cbb
unknown <>
2021-02-27 16:17:56 -0500

    2/27

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -25,10 +25,10 @@ st.title('**Market Basket Analysis**')
 # create a sidebar
 st.sidebar.header('Dataset at a Glance')
 st.sidebar.write('2017 Instacart Data')
-st.sidebar.write('3.4M orders \nline')
-st.sidebar.write('206k unique customer IDs \nline')
-st.sidebar.write('34M rows \nline')
-st.sidebar.write('50k unique products \nline')
+st.sidebar.write('3.4M orders \n')
+st.sidebar.write('206k unique customer IDs \n')
+st.sidebar.write('34M rows \n')
+st.sidebar.write('50k unique products \n')
 
  # Add project details, number of orders, number of unique users, top products, busy and slow days etc.
 
@@ -81,7 +81,7 @@ st.image('../assets/background.jpg')
 
 
 # def main():
-#     html_temp = """<div class='tableauPlaceholder' id='viz1614215722694' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;al&#47;all_orders_dashboard&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='all_orders_dashboard&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;al&#47;all_orders_dashboard&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1614215722694');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.minWidth='420px';vizElement.style.maxWidth='650px';vizElement.style.width='100%';vizElement.style.minHeight='587px';vizElement.style.maxHeight='887px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.minWidth='420px';vizElement.style.maxWidth='650px';vizElement.style.width='100%';vizElement.style.minHeight='587px';vizElement.style.maxHeight='887px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
+#     html_temp = """<div class='tableauPlaceholder' id='viz1614371486793' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ma&#47;MarketBasketAnalysis_Departments&#47;DepartmentAnalysis&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='MarketBasketAnalysis_Departments&#47;DepartmentAnalysis' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ma&#47;MarketBasketAnalysis_Departments&#47;DepartmentAnalysis&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1614371486793');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1416px';vizElement.style.height='991px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
 #     components.html(html_temp)
 #
 # if __name__ == "__main__":
